0810c3b3ef74539964abcdb62b0799c63f72efb6
Spencer Pearson <suspense@cs.washington.edu>
2017-09-02 21:58:33 -0700

    use ES6 classes for hypotheses

--- petrovday/server/static/hypothesis.js
+++ petrovday/server/static/hypothesis.js
@@ -1,20 +1,19 @@
-var Hypothesis = (function() {
+ALPHA = 0.1;
 
+class Hypothesis {
 
-  ALPHA = 0.1;
-  MISSILE_FRACTIONAL_READING_P = function(dt) {return dt/MISSILE_FLIGHT_TIME_SECONDS;};
-
-  function Hypothesis(launchTime) {
+  constructor(launchTime, missileFlightTime) {
     this.launchTime = launchTime;
+    this.missileFlightTime = missileFlightTime;
     this.logRelativeRepresentativeness = 0;
   }
 
-  Hypothesis.prototype.updateRelativeRepresentativeness = function(t, x) {
+  updateRelativeRepresentativeness(t, x) {
     var self = this;
     if (t < self.launchTime) {
       return;
     } else {
-      var p = ALPHA + (1-ALPHA)*MISSILE_FRACTIONAL_READING_P(t-self.launchTime);
+      var p = ALPHA + (1-ALPHA)*(t-self.launchTime)/self.missileFlightTime;
       if (p > 1) {
         self.logRelativeRepresentativeness = null;
       } else {
@@ -22,27 +21,26 @@ var Hypothesis = (function() {
       }
     }
   };
+}
 
-  return Hypothesis;
-})()
-
-var HypothesisSet = (function() {
-
-  function range(start, stop) {
-    var result = new Array(stop-start);
-    for (var i=start; i<stop; i++) {
-      result[i-start] = i;
-    }
-    return result;
+function range(start, stop) {
+  var result = new Array(stop-start);
+  for (var i=start; i<stop; i++) {
+    result[i-start] = i;
   }
+  return result;
+}
 
-  function HypothesisSet() {
+class HypothesisSet {
+
+  constructor(missileFlightTime) {
+    this.missileFlightTime = missileFlightTime;
     this.hypotheses = {};
   }
 
-  HypothesisSet.prototype.updateRelativeRepresentativenesses = function(t, x) {
+  updateRelativeRepresentativenesses(t, x) {
     var self = this;
-    self.hypotheses[t] = new Hypothesis(t);
+    self.hypotheses[t] = new Hypothesis(t, self.missileFlightTime);
     Object.getOwnPropertyNames(self.hypotheses).forEach(function(t_h) {
       self.hypotheses[t_h].updateRelativeRepresentativeness(t, x);
       if (self.hypotheses[t_h].logRelativeRepresentativeness === null) {
@@ -51,11 +49,11 @@ var HypothesisSet = (function() {
     });
   };
 
-  HypothesisSet.prototype.getLogRepresentativeness = function(t) {
+  getLogRepresentativeness(t) {
     return self.hypotheses.hasOwnProperty(i) ? self.hypotheses[i].logRelativeRepresentativeness : null;
   }
 
-  HypothesisSet.prototype.getMaxLogRelativeRepresentativenessInRange = function(start, stop) {
+  getMaxLogRelativeRepresentativenessInRange(start, stop) {
     var self = this;
     var result = undefined;
     return Math.max.apply(null,
@@ -64,6 +62,4 @@ var HypothesisSet = (function() {
         .filter(function(x){return x !== null;})
     );
   };
-
-  return HypothesisSet;
-})();
+}
